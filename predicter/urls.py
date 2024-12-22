126f68560725b13ee49f4cc99888d0a5717e397d
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-12-22 13:43:00 +0700

    update: added public predict api and refactored media processing process

--- predicter/urls.py
+++ predicter/urls.py
@@ -6,4 +6,15 @@ urlpatterns = [
     path('applications/', views.applications_view, name='applications'),
     path('applications/new/', views.create_application, name='create-application'),
     path('applications/<int:id>/', views.application_detail, name='application-detail'),
+    path(
+        'applications/<int:application_id>/update-status',
+        views.update_application_status,
+        name='update-application-status'
+    ),
+    path(
+        'applications/<int:application_id>/change-key',
+        views.change_key,
+        name='change-key'
+    ),
+    path('applications/predict/', views.predict_service, name='predict'),
 ]
--- predicter/views.py
+++ predicter/views.py
@@ -3,6 +3,7 @@ import uuid
 import ffmpeg
 
 from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -10,63 +11,88 @@ from django.utils import timezone
 from django.contrib import messages
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Media, Application
 from .services import frames_extraction, predict_video
 
-@login_required
-def predict_view(request):
-    user_uploaded_media = Media.objects.filter(user=request.user).order_by('-created_at')
-    for media in user_uploaded_media:
-        media.result_slug = media.result.lower().replace(' ', '_')
-    context = {
-        "uploaded_media": user_uploaded_media
-    }
 
+def media_process(file, user, application):
     """
     Process uploaded file
     1. Generate a random UUID v4 file name for the file.
     2. Save the file in chunk to the local file system.
     3. Save the file data in database.
     """
+
+    file_extension = os.path.splitext(file.name)[1]
+    unique_filename = f"{uuid.uuid4()}{file_extension}"
+
+    try:
+        save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', unique_filename)
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        metadata = ffmpeg.probe(save_path)
+        duration = float(metadata['format']['duration'])  # Duration in seconds
+        video_stream = next((stream for stream in metadata['streams'] if stream['codec_type'] == 'video'), None)
+        resolution = f"{video_stream['width']}x{video_stream['height']}" if video_stream else "Unknown"
+
+        response = predict_video(save_path)
+
+        created_media = Media.objects.create(
+            name=unique_filename,
+            original_name=file.name,
+            local_path=f'{settings.MEDIA_URL}uploads/{unique_filename}',
+            size_byte=file.size,
+            duration=duration,
+            resolution=resolution,
+            result="Non Violence" if response.get("predicted_class") == "NonViolence" else "Violence",
+            user=user,
+            application=application,
+        )
+        return (
+            None,
+            {
+                "media_id": created_media.id,
+                "media_name": created_media.name,
+                "predicted_class": response.get("predicted_class"),
+                "predicted_confidence": response.get("confidence"),
+            }
+        )
+    except Exception as e:
+        return e, None
+
+
+@login_required
+def predict_view(request):
+    user_uploaded_media = Media.objects.filter(
+        user=request.user,
+        application_id__isnull=True
+    ).order_by('-created_at')
+    for media in user_uploaded_media:
+        media.result_slug = media.result.lower().replace(' ', '_')
+    context = {
+        "uploaded_media": user_uploaded_media
+    }
+
     if request.method == 'POST' and request.FILES.get('file-upload'):
         uploaded_file = request.FILES['file-upload']
 
-        # * Generate unique file name
-        file_extension = os.path.splitext(uploaded_file.name)[1]
-        unique_filename = f"{uuid.uuid4()}{file_extension}"
-
-        try:
-            save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', unique_filename)
-            os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-            # * Save file to /media/uploads location
-            with open(save_path, 'wb+') as destination:
-                for chunk in uploaded_file.chunks():
-                    destination.write(chunk)
-
-            # * Extract video metadata
-            metadata = ffmpeg.probe(save_path)
-            duration = float(metadata['format']['duration'])  # Duration in seconds
-            video_stream = next((stream for stream in metadata['streams'] if stream['codec_type'] == 'video'), None)
-            resolution = f"{video_stream['width']}x{video_stream['height']}" if video_stream else "Unknown"
-
-            # * Use service to predict video
-            response = predict_video(save_path)
-
-            # * Save file data into database
-            Media.objects.create(
-                name=unique_filename,
-                original_name=uploaded_file.name,
-                local_path=f'{settings.MEDIA_URL}uploads/{unique_filename}',
-                size_byte=uploaded_file.size,
-                duration=duration,
-                resolution=resolution,
-                result="Non Violence" if response.get("predicted_class") == "NonViolence" else "Violence",
-                user=request.user
-            )
-
-            user_uploaded_media = Media.objects.filter(user=request.user).order_by('-created_at')
+        error, _ = media_process(uploaded_file, request.user, None)
+        if error is not None:
+            context = {"status": "error", "message": str(error)}
+        else:
+            user_uploaded_media = Media.objects.filter(
+                user=request.user,
+                application_id__isnull=True
+            ).order_by('-created_at')
             for media in user_uploaded_media:
                 media.result_slug = media.result.lower().replace(' ', '_')
             context = {
@@ -74,11 +100,9 @@ def predict_view(request):
                 "message": "File uploaded successfully!",
                 "uploaded_media": user_uploaded_media
             }
-            print("bug:", user_uploaded_media)
-        except Exception as e:
-            context = {"status": "error", "message": str(e)}
 
     return render(request, 'predict.html', context=context)
+
 
 @login_required
 def applications_view(request):
@@ -95,23 +119,66 @@ def generate_rsa_key_pair():
         key_size=2048  # Key size in bits
     )
 
-    # Serialize the private key to PEM format
+    # * Serialize the private key to PEM format
     private_key_pem = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
         encryption_algorithm=serialization.NoEncryption()
     ).decode('utf-8')
 
-    # Generate public key
+    # * Generate public key
     public_key = private_key.public_key()
 
-    # Serialize the public key to PEM format
+    # * Serialize the public key to PEM format
     public_key_pem = public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ).decode('utf-8')
 
     return public_key_pem, private_key_pem
+
+
+def validate_public_key(public_key_pem, private_key_pem):
+    private_key = serialization.load_pem_private_key(
+        private_key_pem.encode('utf-8'),
+        password=None,
+        backend=default_backend()
+    )
+    public_key = serialization.load_pem_public_key(
+        public_key_pem.encode('utf-8'),
+        backend=default_backend()
+    )
+
+    """
+    # Public key validation algorithm
+    Encrypt a message using the private key to create a signature.
+    Then use that signature to verify the public key. Since only 
+    the correct public key can decrypt the signature. 
+    """
+    message = b"Message for key validation"
+    signature = private_key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    try:
+        public_key.verify(
+            signature,
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return True
+    except Exception as e:
+        print(f"Key validation failed: {e}")
+        return False
+
 
 @login_required
 def create_application(request):
@@ -137,6 +204,98 @@ def create_application(request):
 
     return redirect('/services/applications/')
 
+
+@login_required
 def application_detail(request, id):
     application = Application.objects.get(id=id)
     return render(request, 'application-detail.html', context={'application': application})
+
+
+@login_required
+def update_application_status(request, application_id):
+    if request.method == 'POST':
+        status = request.POST.get('status')
+
+        if application_id and status:
+            application = Application.objects.get(id=application_id)
+            application.status = status
+            application.save()
+            messages.success(request, "Application updated successfully.")
+            return redirect(f'/services/applications/{application.id}')
+
+    return redirect(f'/services/applications/{application_id}')
+
+
+@login_required
+def change_key(request, application_id):
+    if request.method == 'GET':
+        if application_id:
+            application = Application.objects.get(id=application_id)
+            public_key, private_key = generate_rsa_key_pair()
+            application.private_key = private_key
+            application.public_key = public_key
+            application.save()
+            messages.success(request, "Application updated successfully.")
+            return redirect(f'/services/applications/{application.id}')
+
+    return redirect(f'/services/applications/{application_id}')
+
+
+def reformat_public_key_from_request(raw_public_key):
+    public_key = str()
+    for data in raw_public_key.split('\n'):
+        if data == '-----BEGIN PUBLIC KEY-----' or data == '-----END PUBLIC KEY-----':
+            continue
+
+        data = data.replace(" ", "+")
+        public_key = public_key + data + '\n'
+    public_key = '-----BEGIN PUBLIC KEY-----\n' + public_key + '-----END PUBLIC KEY-----\n'
+    return public_key
+
+
+@csrf_exempt
+def predict_service(request):
+    if request.method == 'POST' and request.FILES.get('file-upload'):
+        uploaded_file = request.FILES['file-upload']
+        application_id = request.GET.get('application-id')
+        raw_public_key_pem = request.GET.get('public-key', '')
+        public_key_pem = reformat_public_key_from_request(raw_public_key_pem)
+
+        try:
+            application = Application.objects.get(id=application_id)
+            user = application.user
+        except Application.DoesNotExist:
+            return JsonResponse(
+                {"status": "error", "message": "Invalid application ID."},
+                status=400
+            )
+
+        private_key_pem = application.private_key
+
+        is_valid_public_key = validate_public_key(public_key_pem, private_key_pem)
+        if not is_valid_public_key:
+            return JsonResponse(
+                {"status": "error", "message": "Unauthorized public key"},
+                status=401
+            )
+
+        error, data = media_process(uploaded_file, user, application)
+        if error is not None:
+            return JsonResponse(
+                {"status": "error", "message": "Cannot process file."},
+                status=400
+            )
+
+        return JsonResponse(
+            {
+                "status": "success",
+                "code": 200,
+                "message": "File uploaded successfully!",
+                "data": data,
+            },
+            status=200
+        )
+    return JsonResponse(
+        {"status": "error", "message": "Invalid request or missing file."},
+        status=400
+    )
